f46082ce2493584f7b6df6ab844e69a1c525f52a
Michael Bozada <michaelbozada@gmail.com>
2021-03-05 10:59:02 -0500

    Made Bot
    Bot can run in the testing server.

---
+++ Cogs/Default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from discord.ext import commands
+
+
+class Default(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def hello(self, ctx):
+        await ctx.send(f'Hello!')
+
+    @commands.command()
+    async def ping(self, ctx):
+        latency = round(self.bot.latency * 1000)
+        await ctx.send(f'{latency} ms')
+
+    @commands.command()
+    async def kill(self, ctx):
+        await ctx.bot.logout()
+        exit()
